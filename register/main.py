4154a8d92871e44033e60934699733c0315e2d64
Bruno <brunobraga@protonmail.com>
2025-02-25 18:25:24 -0300

    Send a msg from agent_handler to register with data from netlogo

--- register/main.py
+++ register/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import pika
 import sys
 import os
@@ -12,10 +13,10 @@ def main():
     channel.queue_declare(queue='register')
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+        print(f" [x] Received {body.decode()}")
 
     channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
+        queue='register', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
