a0067666c3abc3d5425fb95c6be6abfa67c6030f
Bruno <brunobraga@protonmail.com>
2025-02-26 15:53:41 -0300

    Insert agent on the database

---
+++ register/database.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+import psycopg2
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    database=os.getenv('DB_DATABASE'),
+    user=os.getenv('DB_USERNAME'),
+    host=os.getenv('DB_HOST'),
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT')
+)
+
+
+def insert_agent(data):
+    sql = """
+        INSERT INTO agents (type_id, data, path)
+            VALUES (%s, %s::vector, %s)
+    """
+
+    conn.cursor().execute(sql, data)
+    conn.commit()
--- register/main.py
+++ register/main.py
@@ -3,22 +3,43 @@
 import pika
 import sys
 import os
+import json
+import random
+
+from database import insert_agent
 
 
 def main():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='omas_rabbitmq'))
+        pika.ConnectionParameters(host='omas_rabbitmq')
+    )
     channel = connection.channel()
 
     channel.queue_declare(queue='register')
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body.decode()}")
+        print(f"Received {body.decode()}")
+
+        data = json.loads(body.decode())
+
+        arr = []
+        for number in range(data['min'], data['max']):
+            arr.append(random.randint(5, 25))
+            arr.append(random.randint(1, 4))
+            arr.append(random.randint(1, 6))
+
+        data = [data['type_id'], str(arr), '']
+
+        insert_agent(data)
 
     channel.basic_consume(
-        queue='register', on_message_callback=callback, auto_ack=True)
+        queue='register',
+        on_message_callback=callback,
+        auto_ack=True
+    )
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
+
     channel.start_consuming()
 
 
