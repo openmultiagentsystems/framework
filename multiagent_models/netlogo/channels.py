76fa870fb702b1cd521ba0499e4eb5d6c7502abd
Bruno <brunobraga@protonmail.com>
2025-03-06 15:57:18 -0300

    Clean up ignored files

---
+++ multiagent_models/netlogo/channels.py
@@ -0,0 +1,24 @@
+import pika
+import json
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='omas_rabbitmq')
+)
+
+channel = connection.channel()
+channel.queue_declare(queue='register')
+
+data = json.dumps({
+    "type_id": 1,
+    "model": 'from_script',
+    "min": 1,
+    "max": 5
+})
+
+channel.basic_publish(
+    exchange='',
+    routing_key='register',
+    body=data
+)
+
+connection.close()
