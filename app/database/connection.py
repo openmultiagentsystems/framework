39e5f021d49cd5ce6c052109871e3a2fba1df9f6
Bruno <brunobraga@protonmail.com>
2025-03-14 09:53:57 -0300

    Update insert to actually save data

--- app/database/connection.py
+++ app/database/connection.py
@@ -57,7 +57,7 @@ def insert_to_router(model_data):
 def insert_to_alive(model_data):
 
     table = Table('alive_agents')
-    q = Query.into(table).columns('agent_id', 'model').insert('1', 'm1')
+    q = Query.into(table).columns('agent_id', 'model').insert(model_data.agent_id, model_data.model)
 
     for agent_id in model_data.agent_id.split(','):
         q = q.insert((agent_id, model_data.model))
