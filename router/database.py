55d85bc686671b8c1d6977501788f8a5f8e0b3ab
Bruno <brunobraga@protonmail.com>
2025-03-31 23:26:54 -0300

    Add autocommit

--- router/database.py
+++ router/database.py
@@ -1,21 +1,30 @@
 import os
 
-from dotenv import load_dotenv
-
 import psycopg2
+from dotenv import load_dotenv
 from psycopg2.extras import execute_values
 
 load_dotenv()
 
-conn = psycopg2.connect(
-    database=os.getenv('DB_DATABASE'),
-    user=os.getenv('DB_USERNAME'),
-    host=os.getenv('DB_HOST'),
-    password=os.getenv('DB_PASSWORD'),
-    port=os.getenv('DB_PORT')
-)
+def pg_connect():
+    conn = psycopg2.connect(
+        database=os.getenv('DB_DATABASE'),
+        user=os.getenv('DB_USERNAME'),
+        host=os.getenv('DB_HOST'),
+        password=os.getenv('DB_PASSWORD'),
+        port=os.getenv('DB_PORT'),
+        options="-c tcp_keepalives_idle=60 -c tcp_keepalives_interval=10 -c tcp_keepalives_count=5"
 
-cursor = conn.cursor()
+    )
+
+    cursor = conn.cursor()
+
+    conn.autocommit = True
+
+    return conn, cursor
+
+
+conn, cursor = pg_connect()
 
 
 def get_router_data():
@@ -27,4 +36,6 @@ def get_router_data():
         return cursor.fetchall()
     except:
         return []
-
+    finally:
+        print('finally')
+        # conn.close()
