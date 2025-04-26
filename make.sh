7773733e99d6a1e5516969f1076d0997b198a001
Bruno <brunobraga@protonmail.com>
2025-04-26 17:50:52 -0300

    Add command to create test

--- make.sh
+++ make.sh
@@ -5,6 +5,9 @@
 # Streamlines most used commands of the project
 
 case "$1" in
+    add-test)
+        docker compose exec php bin/console make:test $@
+        ;;
     setup-test)
         docker compose exec php bin/console doctrine:database:drop --env=test --force
         docker compose exec php bin/console doctrine:database:create --env=test
@@ -26,7 +29,7 @@ case "$1" in
         docker compose exec "$2" /bin/bash -c "python3 /multiagent_models/netlogo/main.py"
         ;;
     *)
-        echo "Usage: $0 {console|migrate|seed|run-model} <service>"
+        echo "Usage: $0 {setup-test|test|console|migrate|seed|run-model} <service>"
         exit 1
         ;;
 esac
