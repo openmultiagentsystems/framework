276b25139963b3fdbabd00a30bc4666496d41706
Bruno <brunobraga@protonmail.com>
2025-05-10 23:34:37 -0300

    Get .nlogo files

--- getmodels.php
+++ getmodels.php
@@ -1,18 +1,32 @@
 <?php
 
 $modelsPaths = [
-    __DIR__ . '/models/',
+    __DIR__ . '/multiagent_models/',
 ];
 
 $db = [];
 function rd($path, $db)
 {
+    $files = [
+        '.',
+        '..',
+        'Dockerfile',
+        '__pycache__',
+        'sugar-map.txt',
+        'py',
+        'agent_handler.py',
+        'agenthandler.nls',
+        'netlogo.log',
+        'main.py'
+    ];
+
     $platforms = array_diff(scandir($path), ['.', '..']);
     $folderName = array_pop($platforms);
 
-    $models = array_diff(scandir($path . $folderName), ['.', '..']);
+    $models = array_diff(scandir($path . $folderName), $files);
     foreach ($models as $model) {
-        $r = array_diff(scandir($path . $folderName . '/' . $model), ['.', '..']);
+        $r = array_diff(scandir($path . $folderName . '/' . $model), $files);
+        $db[] = array_pop($r);
     }
 }
 
